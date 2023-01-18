8f02be4f71439e4a97dc95f8c8bcae294a3ff2f8
archibaldsears <121268618+archibaldsears@users.noreply.github.com>
2023-01-18 02:49:38 -0800

    housekeeping

--- src/materialComponents/twoColumns/ImageWithText.js
+++ src/materialComponents/twoColumns/ImageWithText.js
@@ -1,19 +1,6 @@
-import { motion } from "framer-motion";
-import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
-import Button from "../../components/Button";
+import { MDBRow } from "mdb-react-ui-kit";
 import ImgCol from "../columns/ImgCol";
 import TextCopyBtnCol from "../columns/TextCopyBtnCol";
-import placeholder from "../../images/placeholder@4x.png";
-
-// function ImgCol(props) {
-//   return (
-//     <MDBCol md="6" sm="12" className="mb-4 p-4 align-self-center">
-//       <div>
-//         <motion.img src={props.image}></motion.img>
-//       </div>
-//     </MDBCol>
-//   );
-// }
 
 const ImageWithText = ({
   direction,
